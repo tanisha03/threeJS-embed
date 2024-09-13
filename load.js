2df7b194b9813a9d143cd919fa1ea72dfcebb4cb
Tanisha Sabherwal <tanisha031199@gmail.com>
2024-09-13 21:58:35 +0530

    Create load.js

---
+++ load.js
@@ -0,0 +1,25 @@
+function loadScript(url, callback) {
+  let script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = url;
+
+  // Execute the callback function after the script is loaded
+  script.onload = callback;
+
+  document.head.appendChild(script);
+}
+
+// Load the first script
+loadScript('https://cdn.jsdelivr.net/npm/three@0.139.0/build/three.min.js', function() {
+  
+  // Load the second script after the first one has loaded
+  loadScript('https://cdn.jsdelivr.net/npm/three@0.139.0/examples/js/loaders/GLTFLoader.js', function() {
+    
+    // Load your main script after both scripts are loaded
+    loadScript('https://cdn.jsdelivr.net/gh/tanisha03/threeJS-embed@main/index.js', function() {
+      console.log('All scripts loaded!');
+    });
+
+  });
+
+});
